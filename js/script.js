8d9ba4be2b0e6efc894474e5b37520d6193d128a
davidbuyck <47149695+davidbuyck@users.noreply.github.com>
2025-07-09 12:54:49 -0500

    Added beta site

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// MOBILE NAV TOGGLE
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks  = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
