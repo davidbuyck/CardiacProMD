d8a2ddd2793353d585462dee20a2b242d61e3118
davidbuyck <47149695+davidbuyck@users.noreply.github.com>
2025-07-09 13:48:07 -0500

    Fixed hamburger menu broken

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+// script.js
+
 // MOBILE NAV TOGGLE
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks  = document.querySelector('.nav-links');
 
 navToggle.addEventListener('click', () => {
+  console.log('hamburger clicked');
   navLinks.classList.toggle('open');
 });
